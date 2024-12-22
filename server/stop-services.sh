cb93684534923e2e9eab191a1f340c7ab80b49cf
wijaya2020 <wijaya181028@gmail.com>
2024-12-22 17:19:50 +0700

    commit2:test3

--- server/stop-services.sh
+++ server/stop-services.sh
@@ -1,4 +1,9 @@
 #/bin/sh
+echo "Stop and remove Docker containers..."
 docker compose stop
 docker compose down
+
+echo "Remove Docker image..."
 docker image rm user-service
+
+echo "Services stop completed!"
